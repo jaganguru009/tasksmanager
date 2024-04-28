057bca8e1c913a01134b5c39c50c9f312f54b30c
Jagan M. Jadhav <jagan.jadhav@loglink.io>
2024-04-28 11:44:51 +0530

    Draft add new to do

--- src/App.js
+++ src/App.js
@@ -3,10 +3,36 @@ import Heading from "./components/Heading";
 import clsx from "clsx";
 import SearchIcon from "./assets/icons/SearchIcon";
 import AddIcon from "./assets/icons/AddIcon";
+import TodoPopup from "./components/ToDoPopup";
+import { useState } from "react";
 
+let basicToDoItem = {
+  index: null,
+  item: {
+    title: "",
+    summary: "",
+  },
+};
 function App() {
+  const [selectedToDo, setSelectedToDo] = useState(basicToDoItem);
+  const [showPopup, setShowPopUp] = useState(false);
+  const [todos, setToDos] = useState([]);
+
+  const handleToUpdate = (toDoData) => {
+    console.log(`todo data`, toDoData);
+    setToDos(toDoData);
+  };
   return (
     <div className="flex min-h-screen items-center bg-gray-50">
+      {showPopup && (
+        <TodoPopup
+          setShowPopUp={setShowPopUp}
+          index={selectedToDo.index}
+          toDoItem={selectedToDo.item}
+          handleToUpdate={(data) => handleToUpdate(data)}
+          todos={todos}
+        />
+      )}
       <div className="mx-auto w-full max-w-3xl px-4 py-6">
         <Heading />
         <div className="pt-5">
@@ -37,6 +63,9 @@ function App() {
 
             <button
               type="button"
+              onClick={() => {
+                setShowPopUp(true);
+              }}
               title="Add New Item"
               className={clsx(
                 "rounded-lg bg-emerald-700 px-4 py-2.5",
---
+++ src/components/ToDoPopup.js
@@ -0,0 +1,107 @@
+import clsx from "clsx";
+import { useState } from "react";
+
+const TodoPopup = ({
+  setShowPopUp,
+  index,
+  toDoItem,
+  todos,
+  handleToUpdate,
+}) => {
+  const [todoValue, setTodoValue] = useState(toDoItem ? toDoItem : {});
+
+  const handleEditTodoItem = (event) => {
+    event.preventDefault();
+    let newToDo = {
+      title: event.target.title.value ? event.target.title.value : "",
+      summary: event.target.summary.value ? event.target.summary.value : "",
+    };
+    if (index === null) {
+      todos.push(newToDo);
+    } else {
+      todos[index] = newToDo;
+    }
+    handleToUpdate(todos);
+    setShowPopUp(false);
+  };
+
+  return (
+    <div
+      className={clsx(
+        "fixed bottom-0 left-0 right-0 top-0 z-50",
+        "flex items-center justify-center bg-gray-700/60 p-4"
+      )}
+    >
+      <div className="w-full max-w-md">
+        <form
+          onSubmit={handleEditTodoItem}
+          className="rounded-lg bg-white shadow"
+        >
+          <div className="p-6">
+            <input
+              required
+              type="text"
+              className={clsx(
+                "w-full bg-gray-50 p-4",
+                "rounded-lg border border-gray-300",
+                "text-gray-900",
+                "focus:border-blue-500 focus:ring-blue-500"
+              )}
+              placeholder="Title"
+              name="title"
+              defaultValue={toDoItem && toDoItem.title}
+            />
+          </div>
+          <div className="p-6">
+            <input
+              required
+              type="text"
+              className={clsx(
+                "w-full bg-gray-50 p-4",
+                "rounded-lg border border-gray-300",
+                "text-gray-900",
+                "focus:border-blue-500 focus:ring-blue-500"
+              )}
+              placeholder="Summary"
+              name="summary"
+              defaultValue={toDoItem && toDoItem.summary}
+            />
+          </div>
+
+          <div
+            className={clsx(
+              "flex items-center justify-center space-x-8 p-4",
+              "rounded-b border-t border-gray-200"
+            )}
+          >
+            <button
+              type="submit"
+              className={clsx(
+                "rounded-lg bg-emerald-700 px-5 py-2.5",
+                "text-center font-medium text-white",
+                "hover:bg-emerald-800 focus:outline-none focus:ring-4 focus:ring-emerald-300"
+              )}
+            >
+              Save
+            </button>
+            <button
+              onClick={() => setShowPopUp(false)}
+              type="button"
+              className={clsx(
+                "bg-white px-5 py-2.5",
+                "rounded-lg border border-gray-200",
+                "font-medium text-gray-500",
+                "hover:bg-gray-100 hover:text-gray-900",
+                "focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200"
+              )}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default TodoPopup;
